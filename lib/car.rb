afd38097e37dbdc6d7316fa3c4818a8f49a1f513
Shraddha Rao <shraddharaom@gmail.com>
2023-01-13 11:48:56 -0600

    commit tree d75b6f41e2e4a8ccaab31cb55919cb39ef5290d1

--- lib/car.rb
+++ lib/car.rb
@@ -1,2 +1,23 @@
 # This loads the code from the vehicle.rb file so we can access its code within this file
 require_relative './vehicle'
+
+class Car < Vehicle
+
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_number)
+  @wheel_size = "medium"
+  @wheel_number = 4
+  end
+
+  def go
+    "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
+
+end
+
+
